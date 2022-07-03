ee35d6936fb41e20ccaa5a95e7b52c2dceb0ed66
thaian161 <thaian161@yahoo.com>
2022-07-02 20:36:21 -0500

    seeding users table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,10 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+
+-- INSERT 2 SELLERS
+INSERT INTO users (name, email, password, seller) VALUES
+('Ann Bui', 'an@bui.com', '123', TRUE),
+('Dan', 'dan@l.com', '123', TRUE);
+
+-- INSERT 1 BUYER
+INSERT INTO users (name, email, password) VALUES
+('Ricardo', 'ricardo@lhl.com', '123');
