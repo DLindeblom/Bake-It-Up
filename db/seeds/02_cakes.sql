e2e34c5403e844d9b8a456dd059251021769e3d0
thaian161 <thaian161@yahoo.com>
2022-07-02 20:46:22 -0500

    seeding cakes table

---
+++ db/seeds/02_cakes.sql
@@ -0,0 +1,11 @@
+INSER INTO cakes (user_id, cake_photo_url, title, description, price)
+VALUES
+( 1,'https://images.unsplash.com/photo-1627834377411-8da5f4f09de8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1001&q=80','Macarron Birthday Cake', 'Yellow Macarron',90),
+
+( 1,'https://images.unsplash.com/photo-1622896784083-cc051313dbab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80', 'Trio Graduation Combo'  ,'A combo of 3 cakes for your graduation party',200),
+
+( 2, 'https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80', 'Dripped Coco Cake'  , 'Fresh dripping chocolate cake',120 );
+
+
+
+
