a969f17e5a59edeb49192db0294a4223955d37d1
thaian161 <thaian161@yahoo.com>
2022-07-04 06:46:13 -0500

    styling cake-display on main-page

--- db/seeds/02_cakes.sql
+++ db/seeds/02_cakes.sql
@@ -1,6 +1,6 @@
 INSERT INTO cakes (user_id, cake_photo_url, title, description, price)
 VALUES
-( 1,'https://images.unsplash.com/photo-1622621746668-59fb299bc4d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=633&q=80','Strawberry Cheesecake', 'Strawberry Cheesecake makw with all fresh ingredient',90),
+( 1,'https://previews.123rf.com/images/dolphytv/dolphytv1711/dolphytv171100001/90955371-sliced-no-bake-strawberry-cheesecake-decorated-with-fresh-berries-and-mint-square.jpg','Strawberry Cheesecake', 'Strawberry Cheesecake makw with all fresh ingredient',90),
 
 ( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu layer'  ,'Tiramisu Cake with fresh fruits',200),
 
