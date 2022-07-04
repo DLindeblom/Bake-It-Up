81257d2e3abe471e20bdf1d659c2a0522d363294
ricardo <ricardomoura1979@gmail.com>
2022-07-04 19:47:33 -0400

    added buyer  router and information from dq.query to collect all featured items

--- routes/main-page.js
+++ routes/main-page.js
@@ -1,14 +1,50 @@
+const { application } = require('express');
 const express = require('express');
 const router  = express.Router();
 
+
+
+
+
 module.exports = (db) => {
   router.get("", (req, res) => {
-    res.render("welcome-page")
+    db.query(`SELECT cakes.id, cakes.title,cakes.photo, cakes.description, users.email as user_email, cakes.status
+    JOIN users ON users.id = cakes
+    WHERE featured = true
+    `)
+    .then(result => {
+    res.render("welcome-page", {cakes: result.rows})
+    })
+
+})
+
+  router.get("/buyer", (req, res) => {
+  db.query(`SELECT cakes.id, cakes.title,cakes.photo, cakes.description, users.email as user_email, cakes.status
+  JOIN users ON users.id = cakes
+  
+  `)
+  .then(result => {
+  res.render("buyer-main-page", {cakes: result.rows})
   })
 
+})
   return router;
 }
 
+
+
+
+
+ router.get("", (req, res) => {
+    db.query(`SELECT cakes.id, cakes.title,cakes.photo, cakes.description, users.email as user_email, cakes.status
+    JOIN users ON users.id = cakes
+    WHERE featured = true
+    `)
+    .then(result => {
+    res.render("welcome-page", {cakes: result.rows})
+    })
+
+})
 
 // move filterItems function  below to appropriate file
 
