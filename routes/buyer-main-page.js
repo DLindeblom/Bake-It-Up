c01285a449f52fac81605e5e36dd738759743bbc
DLindeblom <drdlindeblom@gmail.com>
2022-07-05 00:07:50 +0000

    add getFavCakes function to put on fav cakes page

--- routes/buyer-main-page.js
+++ routes/buyer-main-page.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router  = express.Router();
 
+const getCakeInfo = require("../public/scripts/getCakeInfo")
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    res.render("buyer-main-page")
+
+    getCakeInfo().then((result) => {
+
+      const templateVars = { cakes: result }
+      return res.render("buyer-main-page", templateVars)
+
+    })
+    .catch((err) => {
+      console.log(err)
+      res.send(err)
+    })
+
   })
 
   router.get("/favs", (req, res) => {
@@ -12,3 +25,4 @@ module.exports = (db) => {
 
   return router;
 }
+
