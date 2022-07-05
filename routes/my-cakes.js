b857277b17ce3e6be1a94db54c2d134961fd27e1
DLindeblom <drdlindeblom@gmail.com>
2022-07-05 23:51:16 +0000

    filling out form adds to the database

--- routes/my-cakes.js
+++ routes/my-cakes.js
@@ -24,6 +24,25 @@ module.exports = (db) => {
   })
 
   router.post("/", (req, res) => {
+    const cake_photo_url = req.body.cake_photo_url;
+    const title = req.body.title;
+    const description = req.body.description;
+    const price = req.body.price;
+    const userId = req.cookies.user_id;
+
+    const queryString = `
+    INSERT INTO cakes (user_id, cake_photo_url, title, description, price)
+    VALUES ($1, $2, $3, $4, $5)
+    `
+    const queryParams = [userId, cake_photo_url, title, description, price];
+
+    return db.query(queryString, queryParams)
+      .then(() => {
+        res.redirect("/form")
+      })
+      .catch((err) => {
+        console.log(err)
+      })
 
   })
 
--- routes/sell-a-cake-form.js
+++ routes/sell-a-cake-form.js
@@ -3,7 +3,7 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    console.log(req.body)
+
     res.render("sell-a-cake-form")
 
   })
