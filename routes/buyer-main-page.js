12311a617f8fd36e91f4ffe2e11362520b130938
thaian161 <thaian161@yahoo.com>
2022-07-05 16:16:29 -0500

    add filter button and layout on HTML, CSS

--- routes/buyer-main-page.js
+++ routes/buyer-main-page.js
@@ -1,33 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const getCakeInfo = require("../public/scripts/getCakeInfo");
-const getFavCakes = require('../public/scripts/getFavCakes');
+const getFavCakes = require("../public/scripts/getFavCakes");
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+    console.log("is this working");
+    const value = req.query;
+    console.log(value);
 
-    getCakeInfo().then((result) => {
-
-      const templateVars = { cakes: result };
-      return res.render("buyer-main-page", templateVars);
-
-    })
+    getCakeInfo()
+      .then((result) => {
+        const templateVars = { cakes: result };
+        return res.render("buyer-main-page", templateVars);
+      })
       .catch((err) => {
         console.log(err);
         res.send(err);
       });
-
   });
 
   router.get("/favs", (req, res) => {
-
-
-      res.render("buyer-fav-page")
-
-
+    res.render("buyer-fav-page");
   });
 
   return router;
 };
-
