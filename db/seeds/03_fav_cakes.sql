c55e4da924fd977f0b3169ff1a691437eae202ba
DLindeblom <drdlindeblom@gmail.com>
2022-07-05 15:33:00 +0000

    fix typo on buyer main page, add cakes to my favs

---
+++ db/seeds/03_fav_cakes.sql
@@ -0,0 +1,3 @@
+INSERT INTO fav_cakes(user_id, cake_id)
+
+VALUES (1, 1), (1, 2), (2, 1), (2, 2)
--- routes/buyer-main-page.js
+++ routes/buyer-main-page.js
@@ -1,28 +1,40 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
-const getCakeInfo = require("../public/scripts/getCakeInfo")
+const getCakeInfo = require("../public/scripts/getCakeInfo");
+const getFavCakes = require('../public/scripts/getFavCakes');
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
 
     getCakeInfo().then((result) => {
 
-      const templateVars = { cakes: result }
-      return res.render("buyer-main-page", templateVars)
+      const templateVars = { cakes: result };
+      return res.render("buyer-main-page", templateVars);
 
+    })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+
+  });
+
+  router.get("/favs", (req, res) => {
+
+    const userID = 2
+
+    getFavCakes(userID).then((result) => {
+
+      const templateVars = { cakes: result };
+      res.render("buyer-fav-page", templateVars);
     })
     .catch((err) => {
       console.log(err)
-      res.send(err)
     })
 
-  })
-
-  router.get("/favs", (req, res) => {
-    res.render("buyer-fav-page")
-  })
+  });
 
   return router;
-}
+};
 
