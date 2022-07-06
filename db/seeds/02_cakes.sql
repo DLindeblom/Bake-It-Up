9b2841c33b9ceea8aed8c5f7d9832bdacec21b4d
thaian161 <thaian161@yahoo.com>
2022-07-06 16:56:55 -0500

    eding price range on DB seeds

--- db/seeds/02_cakes.sql
+++ db/seeds/02_cakes.sql
@@ -22,15 +22,51 @@ VALUES
 
 ( 1,'https://data.whicdn.com/images/352314441/original.jpg','Mini Birthday Cake', 'A mini birthday cake with blueberry filling whipped cream',75),
 
-( 1,'https://www.tasteofhome.com/wp-content/uploads/2018/01/Triple-Chocolate-Mousse-Torte_EXPS_SDAM18_212092_C11_29_2b-3.jpg', 'Triple Mousse Torte'  ,'A fancy 3 layers of chocolate mousse torte',185),
+( 1,'https://www.tasteofhome.com/wp-content/uploads/2018/01/Triple-Chocolate-Mousse-Torte_EXPS_SDAM18_212092_C11_29_2b-3.jpg', 'Triple Mousse Torte'  ,'A fancy 3 layers of chocolate mousse torte',120),
 
-( 2, 'https://images.getrecipekit.com/20211213012753-hunter-20gatherer-20gourmet-20chocolate-20cake.png?aspect_ratio=1:1&quality=90', 'Christmas Cake'  , 'Who said we cannot eat Christmas cake all year round?',140 ),
+( 2, 'https://images.getrecipekit.com/20211213012753-hunter-20gatherer-20gourmet-20chocolate-20cake.png?aspect_ratio=1:1&quality=90', 'Christmas Cake'  , 'Who said we cannot eat Christmas cake all year round?',122 ),
 
-( 1,'https://zhangcatherine.com/wp-content/uploads/2021/01/12001200.jpg','Boba Brown Sugar Cake', 'Boba milktea in a cake, yummyyy why not?',99),
+( 1,'https://zhangcatherine.com/wp-content/uploads/2021/01/12001200.jpg','Boba Brown Sugar Cake', 'Boba milktea in a cake, yummyyy why not?',73),
 
 ( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu Layer'  ,'Tiramisu is an elegant and rich layered Italian dessert',200),
 
-( 2, 'https://shortgirltallorder.com/wp-content/uploads/2020/07/lemon-lavender-cake-square.jpg', 'Lemon Lavender Cake'  , 'Lemon cake with a lavender note, sweet and saur blended in just 1 piece',115 );
+( 1, 'https://www.rainbownourishments.com/wp-content/uploads/2021/06/vegan-whole-orange-cake-1-1-500x500.jpg', 'Orange Cake', 'Orange Cake with powder sugar on top with fresh orange jam',91 ),
+
+( 1,'https://letthebakingbegin.com/wp-content/uploads/2013/07/The-Best-Napoleon-Cake-is-made-with-thin-puff-pastry-layers-then-sandwiched-with-rich-and-buttery-custard.-This-Napoleon-dessert-is-one-of-my-familys-favorite-2-500x500.jpg','Napoleon Cake', 'Napoleon cake layered with almond paste instead of cream.',49.8),
+
+( 1,'https://826242.smushcdn.com/1643412/wp-content/uploads/2020/01/Mexcian-Hot-Chocolate-Macarons-2-4.jpg?size=1200x1212&lossy=1&strip=1&webp=1', 'Macarons Marshmallow'  ,'he balance between sweet and nutty will determine the goodness of a macaron',93),
+
+( 1, 'https://images.summitmedia-digital.com/spotph/images/2021/11/26/1-1637894525.jpg', 'Floral Birthday Cake'  , 'Elegant and beautiful floral birthday cake with my fresh fruits',230 ),
+
+( 1, 'https://i0.wp.com/www.society19.com/wp-content/uploads/2021/06/original-1.jpg?fit=1080%2C1080&ssl=1', 'Small Color Block Cake'  , 'Patel color block cake, with strawberry and choco filling',125 ),
+
+( 1, 'https://celebratingsweets.com/wp-content/uploads/2018/06/Strawberry-Shortcake-Cake-1-1.jpg', 'Strawberry Short Cake'  , 'A combo of 7 short cakes for an event up to 10 people',129 ),
+
+( 1, 'https://i.pinimg.com/736x/af/29/a0/af29a0425f71cc4808fed2e23fd41955--pretty-wedding-cakes-wedding-cupcakes.jpg', 'Rose Gold Wedding Cake'  , 'Rosey wedding cake made with real gold',350 ),
+
+( 1, 'https://i.pinimg.com/736x/e6/24/e8/e624e85342ae6e717e43b03b58564cb8.jpg', 'Peach & Bery Trio'  , 'A combo of 3 macaron cakes: peach and berry',195 ),
+
+( 1, 'https://www.itakeyou.co.uk/wp-content/uploads/2021/10/minimalist-cake.jpg', 'Large Customize Cake'  , 'Customize birthday cake made by order', 290 ),
+
+( 2, 'https://images.getrecipekit.com/20220504184633-rwbbundtcake.png?aspect_ratio=1:1&quality=90&', 'Fruity Pound Cake:'  , 'Fruity Pound cake made with blueberry and strawberry',40 ),
+
+( 2, 'https://www.rainbownourishments.com/wp-content/uploads/2021/06/vegan-whole-orange-cake-1-1-500x500.jpg', 'Orange Cake', 'Orange Cake with powder sugar on top with fresh orange jam',60 ),
+
+( 2,'https://letthebakingbegin.com/wp-content/uploads/2013/07/The-Best-Napoleon-Cake-is-made-with-thin-puff-pastry-layers-then-sandwiched-with-rich-and-buttery-custard.-This-Napoleon-dessert-is-one-of-my-familys-favorite-2-500x500.jpg','Napoleon Cake', 'Napoleon cake layered with almond paste instead of cream.',70),
+
+( 2,'https://826242.smushcdn.com/1643412/wp-content/uploads/2020/01/Mexcian-Hot-Chocolate-Macarons-2-4.jpg?size=1200x1212&lossy=1&strip=1&webp=1', 'Macarons Marshmallow'  ,'he balance between sweet and nutty will determine the goodness of a macaron',102),
+
+( 2, 'https://images.summitmedia-digital.com/spotph/images/2021/11/26/1-1637894525.jpg', 'Floral Birthday Cake'  , 'Elegant and beautiful floral birthday cake with my fresh fruits',230 ),
+
+( 2, 'https://i0.wp.com/www.society19.com/wp-content/uploads/2021/06/original-1.jpg?fit=1080%2C1080&ssl=1', 'Large Color Block Cake'  , 'Patel color block cake, with strawberry and choco filling',135 ),
+
+( 2, 'https://celebratingsweets.com/wp-content/uploads/2018/06/Strawberry-Shortcake-Cake-1-1.jpg', 'Strawberry Short Cake'  , 'A combo of 7 short cakes for an event up to 10 people',219 ),
+
+( 2, 'https://i.pinimg.com/736x/af/29/a0/af29a0425f71cc4808fed2e23fd41955--pretty-wedding-cakes-wedding-cupcakes.jpg', 'Rose Gold Wedding Cake'  , 'Rosey wedding cake made with real gold',189 ),
+
+( 2, 'https://i.pinimg.com/736x/e6/24/e8/e624e85342ae6e717e43b03b58564cb8.jpg', 'Peach & Bery Trio'  , 'A combo of 3 macaron cakes: peach and berry',320 ),
+
+( 2, 'https://www.itakeyou.co.uk/wp-content/uploads/2021/10/minimalist-cake.jpg', 'Small Customize Cake'  , 'Customize birthday cake made by order', 190 );
 
 
 
