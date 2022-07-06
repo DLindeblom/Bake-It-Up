f7db867e64eee3030be2cb3708c5088acef6425b
Ricardo Moura <ricardomoura1979@gmail.com>
2022-07-06 19:57:37 +0000

    added a function to find a container with heart icon when a client select as favourite

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,12 @@
 // Client facing scripts here
+
+
+function saveFavourite(event) {
+  const heartElement = event.target //access to the cake-display = container
+  const favIconElement = heartElement.parentNode;
+  const cakeDisplayElement = favIconElement.parentNode; //access to the children
+  console.log("cakes[cake].id", cakeDisplayElement.id)
+  const cakeID = cakeDisplayElement.id
+  addFavCake(1, cakeID)
+}
+
