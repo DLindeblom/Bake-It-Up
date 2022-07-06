3a9b939cb6dc949387c8fd1de51442218c128d9e
thaian161 <thaian161@yahoo.com>
2022-07-06 01:14:53 -0500

    edited seeds in database for a wider range of cake_price

--- db/seeds/02_cakes.sql
+++ db/seeds/02_cakes.sql
@@ -2,27 +2,38 @@ INSERT INTO cakes (user_id, cake_photo_url, title, description, price)
 VALUES
 ( 1,'https://previews.123rf.com/images/dolphytv/dolphytv1711/dolphytv171100001/90955371-sliced-no-bake-strawberry-cheesecake-decorated-with-fresh-berries-and-mint-square.jpg','Strawberry Cheesecake', 'Strawberry Cheesecake make with all fresh ingredient',90),
 
-( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu layer' ,'Tiramisu is an elegant and rich layered Italian dessert',200),
+( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu Layer' ,'Tiramisu is an elegant and rich layered Italian dessert',200),
+
+( 2, 'https://caitsplate.com/wp-core/wp-content/uploads/2020/05/IMG_1396-300x300.jpg', 'Croissant French Toast' , 'Fresh Strawberry croissant baked french toast',35 ),
+
+( 1,'https://bakingamoment.com/wp-content/uploads/2022/02/IMG_0530-cream-puff.jpg', 'Cream Puffs' ,'French choux pastry with sweet and moist filling whipped cream',49),
+
+( 1,'https://i.pinimg.com/736x/f4/db/5f/f4db5f53a9b09f8fe155bfa2ad3c6f9a.jpg', 'Trio Mini Cakes' ,'Combo of trio mini cakes for any events',45),
+
+( 2, 'https://www.littlesweetbaker.com/wp-content/uploads/2017/11/german-chocolate-cake-1-1.jpg', 'Dripped Coco Cake'  , 'Fresh dripping chocolate cake adding fruits on top',120 ),
+
+
+( 1,'https://previews.123rf.com/images/dolphytv/dolphytv1711/dolphytv171100001/90955371-sliced-no-bake-strawberry-cheesecake-decorated-with-fresh-berries-and-mint-square.jpg','Strawberry Cheesecake', 'Strawberry Cheesecake make with all fresh ingredient',90),
+
+( 1, 'https://www.theflavorbender.com/wp-content/uploads/2020/02/Strawberry-cream-puffs-0261-2.jpg', 'Velvet Cream Puffs' , 'Fresh Strawberry croissant baked french toast', 39 ),
+
+( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu Layer'  ,'Tiramisu is an elegant and rich layered Italian dessert',200),
 
 ( 2, 'https://www.littlesweetbaker.com/wp-content/uploads/2017/11/german-chocolate-cake-1-1.jpg', 'Dripped Coco Cake'  , 'Fresh dripping chocolate cake adding fruits on top',120 ),
 
 ( 1,'https://previews.123rf.com/images/dolphytv/dolphytv1711/dolphytv171100001/90955371-sliced-no-bake-strawberry-cheesecake-decorated-with-fresh-berries-and-mint-square.jpg','Strawberry Cheesecake', 'Strawberry Cheesecake make with all fresh ingredient',90),
 
-( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu layer'  ,'Tiramisu is an elegant and rich layered Italian dessert',200),
+( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu Layer'  ,'Tiramisu is an elegant and rich layered Italian dessert',200),
 
 ( 2, 'https://www.littlesweetbaker.com/wp-content/uploads/2017/11/german-chocolate-cake-1-1.jpg', 'Dripped Coco Cake'  , 'Fresh dripping chocolate cake adding fruits on top',120 ),
 
 ( 1,'https://previews.123rf.com/images/dolphytv/dolphytv1711/dolphytv171100001/90955371-sliced-no-bake-strawberry-cheesecake-decorated-with-fresh-berries-and-mint-square.jpg','Strawberry Cheesecake', 'Strawberry Cheesecake make with all fresh ingredient',90),
 
-( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu layer'  ,'Tiramisu is an elegant and rich layered Italian dessert',200),
-
-( 2, 'https://www.littlesweetbaker.com/wp-content/uploads/2017/11/german-chocolate-cake-1-1.jpg', 'Dripped Coco Cake'  , 'Fresh dripping chocolate cake adding fruits on top',120 ),
-
-( 1,'https://previews.123rf.com/images/dolphytv/dolphytv1711/dolphytv171100001/90955371-sliced-no-bake-strawberry-cheesecake-decorated-with-fresh-berries-and-mint-square.jpg','Strawberry Cheesecake', 'Strawberry Cheesecake make with all fresh ingredient',90),
-
-( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu layer'  ,'Tiramisu is an elegant and rich layered Italian dessert',200),
+( 1,'https://assets.bonappetit.com/photos/5b9965be60d00151f019a31a/1:1/w_2560%2Cc_limit/tiramisu-layer-cake-with-mascarpone-mousse-slice.jpg', 'Tiramisu Layer'  ,'Tiramisu is an elegant and rich layered Italian dessert',200),
 
 ( 2, 'https://www.littlesweetbaker.com/wp-content/uploads/2017/11/german-chocolate-cake-1-1.jpg', 'Dripped Coco Cake'  , 'Fresh dripping chocolate cake adding fruits on top',120 );
+
+
 
 
 
