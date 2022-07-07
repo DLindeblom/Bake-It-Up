890d713a5d4916979d70ba76c29c8601a75b1379
DLindeblom <drdlindeblom@gmail.com>
2022-07-07 16:00:58 +0000

    fixed conflict on routes page

--- routes/addFavCake.js
+++ routes/addFavCake.js
@@ -1,27 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-
-
-const addFavCakes = require("../public/scripts/addFavCakes");
-
-module.exports = (db) => {
-  router.post("/:id/", (req, res) => {
-    const cakeID = req.params.id;
-    const userID = req.cookies.user_id;
-    addFavCakes(cakeID, userID)
-      .then(() => {
-        res.redirect("/cakes")
-
-      })
-      .catch((error) => {
-        console.log(error);
-
-      })
-
-
-  });
-=======
 const addFavCake = require("../public/scripts/addFavCake")
 
 module.exports = () => {
@@ -40,6 +18,5 @@ module.exports = () => {
       });
   });
 
->>>>>>> fav-page
   return router;
 };
--- routes/buyer-fav-page.js
+++ routes/buyer-fav-page.js
@@ -1,25 +1,4 @@
 const express = require("express");
-<<<<<<< HEAD
-const router = express.Router();
-
-
-const getFavCakes = require("../public/scripts/getFavCakes");
-
-
-module.exports = (db) => {
-  router.get("/", (req, res) => {
-    const userID = req.cookies.user_id;
-    getFavCakes(userID)
-      .then((result) => {
-        const templateVars = { cakes: result, userID };
-        return res.render("buyer-fav-page", templateVars);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.send(err);
-      });
-  });
-=======
 const getFavCakes = require("../public/scripts/getFavCakes");
 const router = express.Router();
 
@@ -36,6 +15,5 @@ module.exports = (db) => {
       });
   });
 
->>>>>>> fav-page
   return router;
 };
